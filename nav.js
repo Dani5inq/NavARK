f308ec68517915e2c5fd5c573bdb1329655f199f
Dani5inq <yungwizzy18@gmail.com>
2023-07-04 20:01:31 +0100

    NEW Changes

--- nav.js
+++ nav.js
@@ -1,6 +1,25 @@
 const barsElement = document.querySelector(".fa-bars");
 const menu = document.querySelector(".menu");
 
+const btnElement = document.querySelector(".btn");
+const videoElement = document.querySelector(".video-banner");
+const fontAwesome = document.querySelector(".fa-play");
+
 barsElement.addEventListener("click", () => {
   menu.classList.toggle("show-menu");
+});
+
+btnElement.addEventListener("click", () => {
+  if (btnElement.classList.contains("pause")) {
+    btnElement.classList.remove("pause");
+    videoElement.play();
+
+    fontAwesome.classList.add("fa-play");
+    fontAwesome.classList.remove("fa-pause");
+  } else {
+    btnElement.classList.add("pause");
+    videoElement.pause();
+    fontAwesome.classList.add("fa-pause");
+    fontAwesome.classList.remove("fa-play");
+  }
 });
